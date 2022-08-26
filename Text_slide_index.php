7899629dba14e81fb8acf27cab92ea16ede95cbb
Jalpeshv <39324383+Jalpeshv@users.noreply.github.com>
2022-08-26 10:29:31 +0530

    Add files via upload

--- Text_slide_index.php
+++ Text_slide_index.php
@@ -2,10 +2,46 @@
 <head>
 <title> Image/Div Slider</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="slider_demo.css">
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<style>
+  .h{
+    height:100%;
+    background-repeat:no-repeat;
+    background-image: url("https://charusat.ac.in/images/home/charusat1.webp");
+    }
+    .h div{
+    text-align: center;
+    padding-top:130px;
+    }
+    section{
+        display:inline-block;
+        /*border:1px solid black;*/
+        color:rgb(186, 240, 61);
+    
+    }
+    .text-block {
+      position: absolute;
+      bottom: 20px;
+      right: 20px;
+      height:100px;
+      background-color: black;
+      color: white;
+      padding-left: 20px;
+      padding-right: 20px;
+    }
+    .item{
+        text-align:center
+    }
+    
+    .container {
+      position: relative;
+      font-family: Arial;
+    }
+</style>
+
 </head>
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
@@ -20,20 +56,20 @@
   <div class="carousel-inner">
     <div class="item active">
       <section>
-            Some Info 1	
+            <span style="z-index: 6; font-size:60px; color:#fff; font-family: 'Montserrat', sans-serif;, serif; white-space: nowrap;font-weight:700;">Demo Data with the text Will Be Shown Here</span>
         </section>
         <br/>
         <section>
-            <h3>Some Info 2</h3>
+          <span style="z-index: 6; font-size:60px; color:#fff; font-family: 'Montserrat', sans-serif;, serif; white-space: nowrap;font-weight:700;">Demo Data with the text Will Be Shown Here</span>
         </section>
         <br/>
         <section>
-            <h4>Some Info 3</h4>
+           <b> <h4>Demo Data with the text Will Be Shown Here</h4></b>
         </section>
     </div>
     <div class="item">
         <section>
-            <h4>Some Info 4</h4>
+          <span style="z-index: 6; font-size:60px; color:#fff; font-family: 'Montserrat', sans-serif;, serif; white-space: nowrap;font-weight:700;">Demo Data with the text Will Be Shown Here</span>
         </section>
         <br/>
         <section>
@@ -41,7 +77,7 @@
         </section>
         <br/>
         <section>
-            Some Info 6
+          <span style="z-index: 6; font-size:60px; color:#fff; font-family: 'Montserrat', sans-serif;, serif; white-space: nowrap;font-weight:700;">Demo Data with the text Will Be Shown Here</span>
         </section>
     </div>
 </div>   
